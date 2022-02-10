dd058ba10bcb43fa30911b67a31aaa02732f7d5f
torik <riqibrekele@gmail.com>
2022-02-10 15:39:15 +0700

    importing styles

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,15 @@
 import '../styles/globals.css'
+import '../styles/var.css'
+
+import '../styles/header/header.css'
+import '../styles/header/nav.css'
 
 import '../styles/btn.css'
+import '../styles/box.css'
 import '../styles/container.css'
 import '../styles/data.css'
 import '../styles/form.css'
 import '../styles/footer.css'
-import '../styles/header.css'
 
 
 
