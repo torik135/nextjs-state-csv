04eda5e5906ef1f98e62367ffaf44c58473de765
torik <riqibrekele@gmail.com>
2022-02-10 15:38:01 +0700

    styling

--- components/header/Header.jsx
+++ components/header/Header.jsx
@@ -1,10 +1,14 @@
+import Heading from "./Heading"
 import Nav from "./Nav"
 
 const Header = () => {
     return (
-        <div>
+        <header className="header">
+            <Heading>
+                <h2>Header</h2>
+            </Heading>
             <Nav/>
-        </div>
+        </header>
     )
 }
 
